2818fd69e8912d5f154f8fe0c6123998e0651152
vishaltyagi <vishaltyagi@vishaltyagi.nppgl.com>
2017-11-16 17:27:32 +0530

    s

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,6 +11,34 @@ class GamesController < ApplicationController
 	# 	redirect_to root_path
 	# end
 
+
+def get_all_tweets
+	@client ||= Twitter::REST::Client.new do |config|
+		config.consumer_key        = "AcDdxJser1b9PZKXO4nkmdAeT"
+		config.consumer_secret     = "spDfoQ40iwcPWJbcy6TzXy0boYTLuuiwqWJ8MowQmKIzsRFg7s"
+		config.access_token        = "929731111697104896-dD3YwJsgkfSuHGkQ4i5ZABqIAhuu89z"
+		config.access_token_secret = "vIy4z6c3YrMtJD9PlEBZJoBS9wkNtsbjnhOR4VHBkhXtO"
+		end
+	#set max_id to max value
+	mnid = 999999999999999999
+	#set number of tweets you want to get 
+	max_no_tweets = 2
+	count = 0
+	while(count < max_no_tweets)
+		#each loop gets 200 tweet
+		@client.user_timeline('sachin tendulkar', :count => 200, :max_id => mnid).each do |tweet|
+			# Tweet.create(message: tweet.full_text) 
+			        puts(tweet.text) if tweet.is_a?(Twitter::Tweet)
+
+			# to get the next 200 tweet
+			mnid = [mnid,tweet.id].min
+			count+=1
+		end
+	end
+		redirect_to root_path
+	
+end
+
 	def get_twittes
 		@client ||= Twitter::REST::Client.new do |config|
 		config.consumer_key        = "AcDdxJser1b9PZKXO4nkmdAeT"
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 	root 'games#index'
 	post 'game/:id' => 'games#create_tweet',  as: 'create_tweet'
 	get 'twittes' => 'games#get_twittes',  as: 'get_twittes'
+	get 'get_all_tweets' => 'games#get_all_tweets',  as: 'get_all_tweets'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
